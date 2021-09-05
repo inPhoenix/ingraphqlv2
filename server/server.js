67892d8e4f812b97b6398385b4e23f50753f757d
inPhoenix <spiderk@gmail.com>
2021-09-05 20:19:38 -0300

    General commit (no IDs open) - Modifications for bad implementations

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
-const { gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server")
 
 const typeDefs = gql`
   type Query {
     greeting: String
   }
-`;
+`
 
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL world!",
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen({ port: 9000 }).then(({ url }) => {
+  console.log(`server running at ${url}`)
+})
